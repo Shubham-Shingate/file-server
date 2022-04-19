76fcbad3fc019af79bc378c2586d6beef1e338fb
Matthew Sentell <msentell7@gmail.com>
2022-04-19 17:46:55 -0400

    separated out file ops in file_sys.rs
    Old ones still in as place holder; still need to update main.rs.

--- src/file_sys.rs
+++ src/file_sys.rs
@@ -97,7 +97,7 @@ impl Files{
             files: Vec::new()
         }
     }
-    fn find(&self, s: &String) -> std::result::Result<&FileInfo, FileError>{ // find a fileinfo in db
+    fn find(&self, s: &str) -> std::result::Result<&FileInfo, FileError>{ // find a fileinfo in db
         for i in &self.files{
             if s == &i.filepath{
                 return Ok(i)
@@ -189,7 +189,49 @@ impl Files{
             },
         }
     }
-    pub fn search(&self, term: &str) -> String{
+    fn find_dir(&self, path: &str) -> bool { // find a directory in db
+        fs::read_dir(Path::new(path)).is_ok()
+    }
+    fn find_file(&self, path: &str) -> bool { // find a file in db
+        OpenOptions::new().open(Path::new(&path)).is_ok()
+    }
+    pub fn read_file(&self, path: &str) -> Result<File> { // read from file
+        Ok(OpenOptions::new().read(true).open(Path::new(&path))?)
+    }
+    pub fn write_file(&self, path: &str, mut ct: File) -> Result<File> { // write to file
+        if self.find_file(path) { //look for existing file
+                let mut file = OpenOptions::new().read(true).write(true).create(true).open(Path::new(&path))?;
+                copy(&mut ct, &mut file)?;
+                Ok(OpenOptions::new().read(true).open(Path::new(&path))?)
+        }
+        else { // write new file
+            let mut file = OpenOptions::new().read(true).write(true).create(true).open(Path::new(&path))?;
+            copy(&mut ct, &mut file)?;
+            OpenOptions::new().read(true).open(Path::new(&path))
+        }
+    }
+    pub fn copy_file(&self, old_path: &str, new_path: &str) -> Result<File> { // copy original to new location
+        let mut file = OpenOptions::new().read(true).write(true).create(false).open(Path::new(old_path))?;
+        self.write_file(new_path, file)
+    }
+    pub fn move_file(&self, old_path: &str, new_path: &str) -> Result<File> {// copy original to new location, then delete original
+        self.copy_file(old_path, new_path)?;
+        self.delete_file(old_path)?;
+        OpenOptions::new().read(true).open(Path::new(old_path))
+    }
+    pub fn delete_file(&self, path: &str) -> Result<bool> { // remove file
+        fs::remove_file(Path::new(path))?;
+        Ok(true)
+    }
+    pub fn make_directory(&self, path: &str) -> Result<bool> {
+        fs::create_dir_all(Path::new(path))?; // add directory
+        Ok(true)
+    }
+    pub fn remove_directory(&self, path: &str) -> Result<bool> {
+        fs::remove_dir_all(Path::new(path))?; // remove directory
+        Ok(true)
+    }
+    pub fn search(&self, term: &str) -> String {
         let p = fs::read_dir("db").unwrap();
         let mut r = String::new();
         for i in p {
